6562006c4ad8a69add11e934d00e00eeeae98da3
= <soconnor0919@gmail.com>
2023-03-25 14:42:59 -0400

    add modal

--- src/index.php
+++ src/index.php
@@ -30,9 +30,30 @@
 </script>
 
 <body class="d-flex h-100 text-center text-bg-dark">
-    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-        <a href="scripts/reset-counter.php" class="btn btn-light fw-bold border-white bg-white">Reset Counter</a>
+    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3">
+        <button type="button" class="btn btn-light fw-bold border-white bg-white" data-bs-toggle="modal" data-bs-target="#confirmModal">
+            Reset Counter
+        </button>
     </div>
+
+    <div class="modal fade" id="confirmModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Are you sure?</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to reset the counter? If you are reading this, then the answer is no.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <a href="scripts/reset-counter.php" class="btn btn-light fw-bold border-white bg-white">Reset Counter</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="cover-container d-flex justify-content-center w-100 h-100 p-3 mx-auto">
         <div class="align-self-center">
             <h1>It's been:</h1>
