7e97ee4e4d9af1c1e7da0df1adce230baebded78
= <soconnor0919@gmail.com>
2023-03-25 16:15:24 -0400

    Stored password in plaintext

--- src/index.php
+++ src/index.php
@@ -5,6 +5,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 
+<?php
+session_start();
+?>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,12 +25,39 @@
 
     function timestamp() {
         $.ajax({
-            url: 'scripts/get_time_since.php',
+            url: 'scripts/get-time-since.php',
             success: function(data) {
                 $('#timestamp').html(data);
             },
         });
     }
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+        function verificationInput() {
+            const inputs = document.querySelectorAll('#otp > *[id]');
+            for (let i = 0; i < inputs.length; i++) {
+                inputs[i].addEventListener('keydown', function(event) {
+                    if (event.key === "Backspace") {
+                        inputs[i].value = '';
+                        if (i !== 0) inputs[i - 1].focus();
+                    } else {
+                        if (i === inputs.length - 1 && inputs[i].value !== '') {
+                            return true;
+                        } else if (event.keyCode > 47 && event.keyCode < 58) {
+                            inputs[i].value = event.key;
+                            if (i !== inputs.length - 1) inputs[i + 1].focus();
+                            event.preventDefault();
+                        } else if (event.keyCode > 64 && event.keyCode < 91) {
+                            inputs[i].value = String.fromCharCode(event.keyCode);
+                            if (i !== inputs.length - 1) inputs[i + 1].focus();
+                            event.preventDefault();
+                        }
+                    }
+                });
+            }
+        }
+        verificationInput();
+    });
 </script>
 
 <?php
@@ -45,16 +76,30 @@ $message = $lines[array_rand($lines)];
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header text-body">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Are you sure?</h1>
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Verify your identity.</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body text-body">
-                    Are you sure you want to reset the counter? If you are reading this, then the answer is no.
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <a href="scripts/reset-counter.php" class="btn btn-light fw-bold bg-primary text-light">Reset Counter</a>
-                </div>
+                <form action="/scripts/reset-counter.php" method="post">
+                    <div class="modal-body text-body">
+                        <div class="container height-100 d-flex justify-content-center align-items-center">
+                            <div class="position-relative">
+                                <h6>Please enter your identity verification code..</h6>
+                                <div id="otp" class="inputs d-flex flex-row justify-content-center mt-1">
+                                    <input class="m-2 text-center form-control rounded" type="text" id="first" name="first" maxlength="1" />
+                                    <input class="m-2 text-center form-control rounded" type="text" id="second" name="second" maxlength="1" />
+                                    <input class="m-2 text-center form-control rounded" type="text" id="third" name="third" maxlength="1" />
+                                    <input class="m-2 text-center form-control rounded" type="text" id="fourth" name="fourth" maxlength="1" />
+                                    <input class="m-2 text-center form-control rounded" type="text" id="fifth" name="fifth" maxlength="1" />
+                                    <input class="m-2 text-center form-control rounded" type="text" id="sixth" name="sixth" maxlength="1" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Reset</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -62,7 +107,7 @@ $message = $lines[array_rand($lines)];
     <div class="cover-container d-flex justify-content-center w-100 h-100 p-3 mx-auto">
         <div class="align-self-center">
             <h1>It's been:</h1>
-            <p id="timestamp" class="lead"></p>
+            <p id="timestamp" class="lead">Please wait...</p>
             <h3><?= $message ?></h3>
         </div>
     </div>
--- src/scripts/reset-counter.php
+++ src/scripts/reset-counter.php
@@ -1,9 +1,22 @@
 <?php
+session_start();
 
-$cur_unix_time = time();
-$file = fopen("counter.txt", "w") or die("Unable to open file!");
-fwrite($file, $cur_unix_time);
-fclose($file);
+$digit_one = $_POST["first"];
+$digit_two = $_POST["second"];
+$digit_three = $_POST["third"];
+$digit_four = $_POST["fourth"];
+$digit_five = $_POST["fifth"];
+$digit_six = $_POST["sixth"];
+
+$verification_code = $digit_one . $digit_two . $digit_three . $digit_four . $digit_five . $digit_six;
+$verification_code = (int)$verification_code;
+
+if ($verification_code == 744353) {
+    $cur_unix_time = time();
+    $file = fopen("counter.txt", "w") or die("Unable to open file!");
+    fwrite($file, $cur_unix_time);
+    fclose($file);
+}
 
 header("Location: /index.php");
 
